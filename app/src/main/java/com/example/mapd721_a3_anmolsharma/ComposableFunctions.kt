0db28710683299668573f712946762324b249051
Anmol Sharma <anmolsharma55555@gmail.com>
2024-03-21 08:31:33 +0530

    Button Styling Changes

--- app/src/main/java/com/example/mapd721_a3_anmolsharma/ComposableFunctions.kt
+++ app/src/main/java/com/example/mapd721_a3_anmolsharma/ComposableFunctions.kt
@@ -1,16 +1,21 @@
 package com.example.mapd721_a3_anmolsharma
 
 import android.content.Intent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mapd721_a3_anmolsharma.ui.theme.MAPD721A3AnmolSharmaTheme
+
 
 @Composable
 fun CenteredButtons(onButtonClick: (String) -> Unit) {
@@ -19,17 +24,37 @@ fun CenteredButtons(onButtonClick: (String) -> Unit) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Button(onClick = { onButtonClick("TransitionAnimationActivity") }) {
-            Text("Go to Transition Animation")
+        Button(
+            onClick = { onButtonClick("TransitionAnimationActivity") },
+            modifier = Modifier
+                .fillMaxWidth(0.8f),
+            colors = ButtonDefaults.buttonColors(Color.Gray)
+        ) {
+            Text("Transition Animation")
         }
-        Button(onClick = { onButtonClick("ScaleAnimationActivity") }) {
-            Text("Go to Scale Animation")
+        Button(
+            onClick = { onButtonClick("ScaleAnimationActivity") },
+            modifier = Modifier
+                .fillMaxWidth(0.8f),
+            colors = ButtonDefaults.buttonColors(Color.Blue)
+        ) {
+            Text("Scale Animation")
         }
-        Button(onClick = { onButtonClick("InfiniteAnimationActivity") }) {
-            Text("Go to Infinite Animation")
+        Button(
+            onClick = { onButtonClick("InfiniteAnimationActivity") },
+            modifier = Modifier
+                .fillMaxWidth(0.8f),
+            colors = ButtonDefaults.buttonColors(Color.Red)
+        ) {
+            Text("Infinite Animation")
         }
-        Button(onClick = { onButtonClick("ExitAnimationActivity") }) {
-            Text("Go to Exit Animation")
+        Button(
+            onClick = { onButtonClick("ExitAnimationActivity") },
+            modifier = Modifier
+                .fillMaxWidth(0.8f),
+            colors = ButtonDefaults.buttonColors(Color.Black)
+        ) {
+            Text("Exit Animation")
         }
     }
 }
